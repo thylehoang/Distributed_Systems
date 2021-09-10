7d02174ab727a984a636d15e334227f39237caea
Thy <lehoangthy0198@gmail.com>
2021-09-11 02:08:57 +1000

    add a bit of client program in

--- src/main/java/ChatClient.java
+++ src/main/java/ChatClient.java
@@ -1,16 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class ChatClient {
     private static final int PORT = 6379;
+    private boolean alive = false;
+    private BufferedReader reader;
+    private PrintWriter writer;
+    private Socket clientSocket;
 
     public static void main(String[] args) {
-        ChatClient server = new ChatClient();
+        ChatClient client = new ChatClient();
+        client.connect();
     }
 
-    //The client need to accept input from standard input
-    // # mean command
-    //else message
-    // write output to standard output
+    private void connect() {
+        //Socket clientSocket;
+        try {
+            clientSocket = new Socket(InetAddress.getLocalHost(),PORT);
+            alive = true;
+            while(alive){
+                writer = new PrintWriter(clientSocket.getOutputStream());
+                reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                //maybe need to implement threads to write and read things in....
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-    //newline terminated JSON Objects
-    // UTF8 encoded
 
+    }
 }
